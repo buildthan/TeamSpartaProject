94d4919c173ab546c1eea8c63f0d7b470bee8a77
000SIN <126735143+000SIN@users.noreply.github.com>
2025-02-10 10:27:43 +0900

    FIX
    fix

--- TeamSpartaProject/Program.cs
+++ TeamSpartaProject/Program.cs
@@ -80,13 +80,13 @@ class GameManager
 
     public void MainScreen() //메인메뉴
     {
-        
+
+
+        Console.Clear();
         // 고양이 아트 출력
         ConsoleStyler.PrintCatArt();
 
-        Console.Clear();
-
-        Console.Clear();
+      
         Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
         Console.WriteLine("이제 전투를 시작할 수 있습니다.");
         Console.WriteLine("");
